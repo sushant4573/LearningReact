5a7a8846fdb908b8dee754b54b64da0773c1a4b3
Sushant Rathod <sushant.rathod@hashedin.com>
2019-09-09 15:43:51 +0530

    State change using onClick method

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,19 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './component/Greet'
 import Welcome from './component/welcome'
+import Message from './component/Message'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Greet name='BRUCE WAYNE' heroName='BATMAN' />
-          <h4>I am BatMan</h4>
-        <Greet name='CLARKE KENT' heroName='SUPERMAN' />
-          <h4>I am SuperMan son of Krypton</h4>
-        <Greet name='DIANA PRINCE' heroName='WONDER-WOMAN' />
-          <h4>I am Dianna Prince of themeskera</h4>
-        <Welcome name="JOKER"/>
-        
+        <Message />
       </div>
     );
   }
