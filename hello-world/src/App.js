388ee3d6f5c37e231763cebc162144c4b82d9d7a
Sushant Rathod <sushant.rathod@hashedin.com>
2019-09-10 13:04:51 +0530

    use of CSS and CSS Modules with Components

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import UserGreeting from './component/UserGreeting';
-import NameList from './component/NameList';
-import PersonList from './component/PersonList';
+import './css/styles.css';
+import sty from './css/styles.module.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <PersonList/>
+        <h1 className='error'>Error</h1>
+        <h1 className={sty.success}>Success</h1>
       </div>
     );
   }
