b57ef3c3dd541f28ab2e26a85463482f8ea06cb7
Sushant Rathod <sushant.rathod@hashedin.com>
2019-09-11 14:58:04 +0530

    React.memo implementation. Its a PureComponent to Function Components

--- hello-world/src/component/ParentComp.js
+++ hello-world/src/component/ParentComp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PureComp from './PureComp'
 import RegComp from './RegComp'
+import MemoComp from './MemoComp'
 
  class ParentComp extends Component {
      constructor(props) {
@@ -23,8 +24,9 @@ import RegComp from './RegComp'
         return (
             <div>
                 Parent Components
-                <PureComp name={this.state.name}/>
-                <RegComp name={this.state.name}/>
+                <MemoComp name={this.state.name} />
+                {/* <PureComp name={this.state.name}/>
+                <RegComp name={this.state.name}/> */}
             </div>
         )
     }
