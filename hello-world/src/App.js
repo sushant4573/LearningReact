a5df204c1ea06d802d63a990ef98bfdcc5b4a986
Sushant Rathod <sushant.rathod@hashedin.com>
2019-09-11 15:35:35 +0530

    Refs Directly access DOM elements

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import './css/styles.css';
-import ParentComp from './component/ParentComp';
+import RefsDemo from './component/RefsDemo';
+import FocusInput from './component/FocusInput';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ParentComp />
+       <FocusInput />
       </div> 
     );
   }
